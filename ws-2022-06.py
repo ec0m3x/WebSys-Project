d6b00f6875593043f3a49f0599a773ce17ff3637
Sebastian <skoch2@stud.hs-heilbronn.de>
2022-10-20 07:54:50 +0200

    Login Page basis erstellt

--- ws-2022-06.py
+++ ws-2022-06.py
@@ -1,7 +1,7 @@
 """Minimale Flask Anwendung"""
 
 # Import benötigter Flask-Module
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Import der Verbindungsinformationen zur Datenbank:
 # Variable DB_HOST: Servername des MySQL-Servers
@@ -16,8 +16,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Startseite"""
-    return render_template('homepage.html')
-
+    return render_template('index.html')
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == "POST":
+        #Nutzer hat Formular abgesendet
+        request.form['Nutzername']
+        request.form['Passwort']
+    return render_template("login.html")
 
 # Start der Flask-Anwendung
 if __name__ == '__main__':
