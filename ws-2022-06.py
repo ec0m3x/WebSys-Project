006a651ec58a1150165c50cf97dee75ea2a6aa4b
Sebastian <skoch2@stud.hs-heilbronn.de>
2022-11-08 18:20:13 +0100

    Registrierungsseite erstellt

--- ws-2022-06.py
+++ ws-2022-06.py
@@ -2,6 +2,7 @@
 
 # Import benötigter Flask-Module
 from flask import Flask, render_template, request
+from flask_login import login_user, login_required, logout_user, current_user
 
 # Import der Verbindungsinformationen zur Datenbank:
 # Variable DB_HOST: Servername des MySQL-Servers
@@ -17,28 +18,53 @@ app = Flask(__name__)
 def index():
     """Startseite"""
     return render_template('index.html')
-@app.route('/login', methods=['GET','POST'])
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
-        #Nutzer hat Formular abgesendet
-        request.form['Nutzername']
-        request.form['Passwort']
+        email = request.form.get('email')
+        password = request.form.get('password')
     return render_template("login.html")
+
+
+@app.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+
+    return render_template("sign_up.html")
+
+@app.route('/logout')
+#@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
 @app.route('/skoch')
 def skoch():
     return render_template('skoch.html')
+
 
 @app.route('/oak')
 def oak():
     return render_template('oak.html')
 
+
 @app.route('/dseljaci')
 def dseljaci():
     return render_template('dseljaci.html')
 
+
 @app.route('/csoenmez')
 def csoenmez():
     return render_template('csoenmez.html')
+
 
 # Start der Flask-Anwendung
 if __name__ == '__main__':
