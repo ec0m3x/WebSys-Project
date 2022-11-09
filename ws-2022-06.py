34e1d35dd2e939bd73804eac617a99dbdcd35c8e
Sebastian <skoch2@stud.hs-heilbronn.de>
2022-11-09 11:56:07 +0100

    Datenbank mit SQL Connector anstatt sqlAlchemy angelegt

--- ws-2022-06.py
+++ ws-2022-06.py
@@ -1,24 +1,38 @@
-"""Minimale Flask Anwendung"""
-
 # Import benötigter Flask-Module
-from flask import Flask, render_template, request, flash
-from flask_login import login_user, login_required, logout_user, current_user
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, flash, session, url_for, g, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
+import mysql.connector
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
+# db = SQLAlchemy()
+# DB_NAME = "database.db"
 
 # Import der Verbindungsinformationen zur Datenbank:
 # Variable DB_HOST: Servername des MySQL-Servers
 # Variable DB_USER: Nutzername
 # Variable DB_PASSWORD: Passwort
 # Variable DB_DATABASE: Datenbankname
-#from db.db_credentials import DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE
+from db.db_credentials import DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'la web de la sys'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-db.init_app(app)
+app.secret_key = 'la web de la sys'
+
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+# db.init_app(app)
+@app.before_request
+def before_request():
+    """ Verbindung zur Datenbank herstellen """
+    g.con = mysql.connector.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD,
+                                    database=DB_DATABASE)
+
+
+@app.teardown_request
+def teardown_request(exception):  # pylint: disable=unused-argument
+    """ Verbindung zur Datenbank trennen """
+    con = getattr(g, 'con', None)
+    if con is not None:
+        con.close()
+
 
 @app.route('/')
 def index():
@@ -28,48 +42,108 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        benutzername = request.form.get('benutzername')
-        password = request.form.get('password')
     return render_template("login.html")
 
 
 @app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    #Eingaben des Benutzers
+    # Eingaben des Benutzers in Variablen speichern
     if request.method == 'POST':
-        email = request.form.get('email')
+        username = request.form.get('username')
         first_name = request.form.get('firstName')
         last_name = request.form.get('lastName')
+        email = request.form.get('email')
+        street = request.form.get('strasse')
+        streetnumber = request.form.get('hausnummer')
+        plz = request.form.get('plz')
+        city = request.form.get('stadt')
+        telephone = request.form.get('tel')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+        # Prüfen auf bereits bestehende Einträge
+        cursor = g.con.cursor()
+        cursor.execute('SELECT username FROM users where username= %s', (username,))
+        row = cursor.fetchone()
+        cursor.close()
+        if row is not None:
+            flash("Benutzername ist bereits vergeben", category="error")
+            return redirect(url_for('index'))
 
-        #Eingabe überprüfen
+        cursor = g.con.cursor()
+        cursor.execute('SELECT email FROM users where email= %s', (email,))
+        row = cursor.fetchone()
+        cursor.close()
+        if row is not None:
+            flash("Email ist bereits vergeben", category="error")
+            return redirect(url_for('index'))
+
+        # Eingabe überprüfen
         if len(email) < 4:
             flash('Email muss länger als 4 Zeichen sein!', category='error')
+            return redirect(url_for('index'))
+        elif len(username) < 2:
+            flash('Benutzername muss länger als 2 Zeichen sein', category='error')
+            return redirect(url_for('index'))
         elif len(first_name) < 2:
             flash('Vorname muss länger als 2 Zeichen sein!', category='error')
+            return redirect(url_for('index'))
         elif len(last_name) < 2:
             flash('Nachname muss länger als 2 Zeichen sein!', category='error')
+            return redirect(url_for('index'))
         elif password1 != password2:
             flash('Passwörter stimmen nicht überein!', category='error')
+            return redirect(url_for('index'))
         elif len(password1) < 7:
             flash('Passwort muss länger als 7 Zeichen sein!', category='error')
+            return redirect(url_for('index'))
         else:
-            #Benutzer zur Datenbank hinzufügen
+            # Benutzer zur Datenbank hinzufügen
+            password = generate_password_hash(password1)
+
+            # Nutzer in der Datenbank anlegen
+            cursor = g.con.cursor()
+            cursor.execute(
+                'INSERT INTO users (email, username, firstname, lastname, street, streetnumber, telephone, password) VALUES (%s, %s, %s, %s,%s, %s,%s, %s,)',
+                (username, first_name, last_name, street, streetnumber, plz, city, telephone, password))
+            g.con.commit()
+            cursor.close()
             flash('Account erfolgreich erstellt', category='success')
+            return redirect(url_for('index'))
 
     return render_template("sign_up.html")
 
+
 @app.route('/logout')
-#@login_required
+# @login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
+
+@app.route('/skoch')
+def skoch():
+    return render_template('skoch.html')
+
+
+@app.route('/oak')
+def oak():
+    return render_template('oak.html')
+
+
+@app.route('/dseljaci')
+def dseljaci():
+    return render_template('dseljaci.html')
+
+
+@app.route('/csoenmez')
+def csoenmez():
+    return render_template('csoenmez.html')
+
+
 @app.route('/about_us')
 def about_us():
     return render_template('about_us.html')
+
 
 # Start der Flask-Anwendung
 if __name__ == '__main__':
