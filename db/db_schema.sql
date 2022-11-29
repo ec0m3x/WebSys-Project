ac52efc3fb9585f89ac6bdb8ef2ce50f186200d1
Onur Ak <oak@stud.hs-heilbronn.de>
2022-11-29 14:54:54 +0100

    Komplette SQL Befehle zur Erstellung der Datenbanktabellen sowie einiger Beispieldaten in Datei 'db_schema.sql' dokumentiert

---
+++ db/db_schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE reservations (
+    id int auto_increment not null,
+    starttime datetime not null,
+    endtime datetime not null,
+    userid int not null,
+    tableid int not null,
+    primary key (id),
+    foreign key (userid) references users(id),
+    foreign key (tableid) references table(id)
+);
+
+INSERT INTO reservations
+VALUES (1, '2022-11-30 18:15:00', '2022-11-30 19:00:00', 1, 1);
+
+CREATE TABLE table (
+    id int auto_increment not null,
+    capacity int not null,
+    primary key (id)
+);
+
+INSERT INTO table
+VALUES (1, 7);
+
+CREATE TABLE users (
+    id int auto_increment not null,
+    name varchar (50) not null,
+    email varchar (50) not null,
+    password varchar (256) not null,
+    adminflag tinyint(1) not null,
+    primary key (id)
+);
+
+INSERT INTO users
+VALUES (1, 'maxmustermann', 'maxmustermann@gmail.com', 'passwort')
